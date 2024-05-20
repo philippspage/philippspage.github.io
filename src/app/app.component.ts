48681f9793856cd24ff60d516e03e0b79a047487
philippspage <jugong@web.de>
2024-05-20 06:34:41 +0200

    Update MQTT communiaction with UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,52 +3,68 @@ import { Subscription } from 'rxjs';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'philippspage.github.io';
   private subscription!: Subscription;
-  topicname: any;
-  msg: any;
+  topicname: string = 'philippspage/alarmState';
+  msg: string = '';
   isConnected: boolean = false;
   @ViewChild('msglog', { static: true })
   msglog!: ElementRef;
+  alarmSet: boolean = false;
 
-  constructor(private _mqttService: MqttService) { }
+  constructor(private _mqttService: MqttService, private _snackBar: MatSnackBar) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.subscribeNewTopic();
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
   subscribeNewTopic(): void {
-    console.log('inside subscribe new topic')
     this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
-      this.msg = message;
-      console.log('msg: ', message)
-      this.logMsg('Message: ' + message.payload.toString() + '<br> for topic: ' + message.topic);
+      this.msg = message.payload.toString();
+      console.log('Incomming MQTT message: ' + this.msg);
+      if (this.msg == "1") {
+        this.alarmSet = true;
+        this.openSnackBar('Alarm gesetzt!');
+      } else {
+        this.alarmSet = false;
+        this.openSnackBar('Alarm gelöst!');
+      }
     });
-    this.logMsg('subscribed to topic: ' + this.topicname)
   }
 
-  sendmsg(): void {
+  sendmsg(message: string): void {
     // use unsafe publish for non-ssl websockets
-    this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 0, retain: false })
+    this._mqttService.unsafePublish(this.topicname, message, { qos: 0, retain: true })
     this.msg = ''
   }
 
-  logMsg(message: string): void {
-    this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
+  setAlarm(): void {
+    this.sendmsg("1");
   }
 
-  clear(): void {
-    this.msglog.nativeElement.innerHTML = '';
+  resetAlarm(): void {
+    this.sendmsg("0");
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, 'Schließen', { duration: 5000, panelClass: ['alarm-snackbar'] });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// wss://broker.hivemq.com:8884/mqtt
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   hostname: 'broker.hivemq.com',
   port: 8884,
@@ -12,6 +14,6 @@ export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS)),
+    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS)), provideAnimationsAsync(),
   ]
 }).catch((err) => console.error(err));
